7d41cf9c6975389b2d201aa72f64e605468d5e8d
Bhumika Sharma <bhumikaasharma25@gmail.com>
2026-07-16 23:43:43 +0530

    episode 2

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 /* <div id="parent">
         <div id="child1">
             <h1>Hi i'm h1 tag</h1>
@@ -11,10 +13,14 @@
 
 
     const parent = React.createElement("div",{id:"parent"},
-        [React.createElement("div",{id:"child1"},
-            [React.createElement("h1",{},"Hi i'm h1 tag!"),React.createElement("h2",{},"Hi i'm h2 tag!")]
-        ),React.createElement("div",{id:"child2"},[React.createElement("h1",{},"Hi i'm h1 tag"),React.createElement("h2",{},"Hi i'm h2 tag")])]
+        [React.createElement("div",{id:"child1",key:"child1"},
+            [React.createElement("h1",{key:"h1"},"This is Namaste React"),
+                React.createElement("h2",{key:"h2"},"My name is Bhumika Sharma")]
+        ),
+        React.createElement("div",{id:"child2",key:"child2"},
+            [React.createElement("h1",{key:"h1"},"Hi i'm h1 tag"),
+                React.createElement("h2",{key:"h2"},"Hi i'm h2 tag")])]
     );
 
-    const root = ReactDOM.createRoot(document.querySelector("body"));
+    const root = ReactDOM.createRoot(document.getElementById("root"));
     root.render(parent);
