84143637c105dcca1d89c00e174b0ca746c9d00b
Bhumika Sharma <bhumikaasharma25@gmail.com>
2026-07-22 23:30:01 +0530

    Episode 04

--- app.js
+++ app.js
@@ -1,20 +1,62 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-//React Element
-const heading = React.createElement("h1",{},"Namaste React");
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
 
-//JSX
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMoFsAFa91KNiN2hTitFR100JGgh4X5jFT_m6UjVKHDw&s=10"></img>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+};
 
-const Title = () => (<h1 id="heading" tabIndex="5">Namaste React from JSX</h1>);
-const number = 10000;
+const RestrauntCard = (props) => {
+    return (
+         <div className="res-card">
+            
+                <img className="food-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJmRKRa7Eey9rLl2krJNJ24LVPrDV1RepV2LSM2PrAnJk34Rd6uvrqaKbp&s=10"></img>
+                <h2>{props.resName}</h2>
+                <p>{props.cuisine}</p>
+                <p>⭐️4.3</p>
+                <h4>38 minutes</h4>
+           
+         </div>
+    )
+}
 
-//React Component
-const HeadingComponent = () => (
-    <div id="container">
-        <Title />
-        <h1 className="heading">{number} Functional Component</h1>
+const Body = () => {
+   return (<div className="body">
+        <div className="search-bar">
+            <input type="text" placeholder="Search Restraunt"></input>
+        </div>
+        <div className="restro-container"> 
+            <RestrauntCard resName="Meghna Foods" cuisine = "Biryani,North Indian,Asian" />
+            <RestrauntCard resName = "KFC" cuisine = "Burger,Fast Food" />
+          
+        </div>
     </div>
-);
-root.render(<HeadingComponent/>);
+   )
+}
+const AppLayout = () => {
+    return <div className="app">
+        <Header />
+        <Body />
+    </div>;
+}
+
+
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout/>);
